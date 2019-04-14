826816c16924dc0235989fcd2f38bd56841628c3
Ganesh Chandrasekaran <gchandra@gmail.com>
2019-04-13 22:02:05 -0400

    Add files via upload

---
+++ multiprocessing_countdown.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+__author__ = "Ganesh Chandrasekaran"
+__license__ = "Open Source GPL"
+__version__ = "1.0"
+__status__ = "Development"
+__description__ = "Code to demonstrate MultiProcessing concept"
+
+import datetime
+import multiprocessing
+
+class fn_Countdown(multiprocessing.Process):
+    def __init__(self,processname,count):
+        multiprocessing.Process.__init__(self)
+        self.count = count
+        self.processname = processname
+        
+    def run(self):
+        while self.count > 0:
+            currentts = datetime.datetime.now()
+            print ("timestamp : {} -> {} - {} | ".format(currentts,self.processname,self.count))
+            self.count -= 2
+            time.sleep(2)
+        return
+
+if __name__ == '__main__':
+   process1 = fn_Countdown('Process-1 ', 20)
+   process1.start()                
+
+   process2 = fn_Countdown('Process-2 ',19)
+   process2.start()      
+
+
+# In[ ]:
+
+
+
+
---
+++ threading_countdown.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[53]:
+
+
+__author__ = "Ganesh Chandrasekaran"
+__license__ = "Open Source GPL"
+__version__ = "1.0"
+__status__ = "Development"
+__description__ = "Code to demonstrate Threading concept"
+
+import threading
+import datetime
+
+th_lst = []
+def fn_countdown(thread,count):
+    while count > 0:
+        currentts = datetime.datetime.now()
+        addtolist(str(currentts),thread,count)
+        print ("timestamp : {} -> {} - {} | ".format(currentts,thread, count))
+        count -= 2
+        time.sleep(2)
+    return
+
+def main():
+    #threading t1 and t2
+
+    thread1 = threading.Thread(target=fn_countdown,args=('Thread-1',20))
+    thread1.start()
+
+    thread2 = threading.Thread(target=fn_countdown,args=('Thread-2',19))
+    thread2.start()
+
+def addtolist(ts,thread,count):
+    th_lst.append((ts,thread,count))
+    
+    
+    
+if __name__ == '__main__':
+    main()   
+
+
+# In[ ]:
+
+
+
+
